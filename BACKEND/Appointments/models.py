9d74325bcea4ed5a656bd10a17b94e955b94dcc6
Empress0205 <zakianaswabu@gmail.com>
2025-12-05 00:29:26 +0300

    made changes in models.py

--- BACKEND/Appointments/models.py
+++ BACKEND/Appointments/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Students(models.Model):
+    username=models.CharField(max_length=200, unique=True)
+    password=models.CharField(max_length=100)
+    first_login=models.BooleanField(default=True)#this will help in validation of whether the students have login for the first time
+ 
+    def __str__(self):
+        return self.username
