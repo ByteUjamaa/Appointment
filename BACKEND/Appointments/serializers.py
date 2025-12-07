b1523a211abee98a331d175e9591aa9b5b593fb3
KIWOLY <ikiwoly@gmail.com>
2025-12-07 15:16:13 +0300

    Update serializer for appointment-types

--- BACKEND/Appointments/serializers.py
+++ BACKEND/Appointments/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from .models import AppointmentType
 
 class AppointmentTypeSerializer(serializers.ModelSerializer):
+    value = serializers.CharField(source='name', read_only=True)  
+    label = serializers.CharField(source='name', read_only=True)  
+
     class Meta:
         model = AppointmentType
-        fields = ['id', 'name']  
+        fields = ['id', 'value', 'label']
--- BACKEND/Appointments/urls.py
+++ BACKEND/Appointments/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import list_appointment_types
 
 urlpatterns = [
-    path('appointment-types/', list_appointment_types, name="appointment-types"),
+    path('types/', list_appointment_types, name="appointment-types"),
 ]
