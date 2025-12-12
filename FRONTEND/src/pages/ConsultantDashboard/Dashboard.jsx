43f334cbf4d2ef7a80971887c36a00de972e3a10
SHABAN171 <shabanindekupe@gmail.com>
2025-12-12 08:20:03 +0300

    final dashboard updates

--- FRONTEND/src/pages/ConsultantDashboard/Dashboard.jsx
+++ FRONTEND/src/pages/ConsultantDashboard/Dashboard.jsx
@@ -1,27 +1,32 @@
 import React, { useState } from "react";
-import {  Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
-
-
 
 const Dashboard = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="flex min-h-screen bg-gray-100">
+    <div className="flex h-screen overflow-hidden bg-gray-100">
 
-     {/* SIDEBAR */}
-      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+      {/* SIDEBAR */}
+      <div className="h-full">
+        <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+      </div>
 
-      {/*  MAIN CONTENT AREA */}
-      <div className="flex-1 ">
-  <Header isOpen={isOpen} setIsOpen={setIsOpen} />
+      {/* MAIN CONTENT */}
+      <div className="flex-1 flex flex-col overflow-hidden">
 
-      
-       <div className="pt-[80px] pl-0  pb-6">
-  <Outlet/>
-</div>
+        {/* STICKY HEADER */}
+        <div className="sticky top-0 z-20 bg-white shadow">
+          <Header isOpen={isOpen} setIsOpen={setIsOpen} />
+        </div>
+
+        
+        <div className="flex-1 overflow-y-auto px-6 pb-6 pt-20">
+          <Outlet />
+        </div>
+
       </div>
     </div>
   );
