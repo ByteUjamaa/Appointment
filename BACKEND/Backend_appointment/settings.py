5a1c013bc74c3ca7ff26130f57df0796817536d7
Samia <hamadsamia37@gmail.com>
2026-01-23 00:19:52 +0300

    dockerized backend and db

--- BACKEND/Backend_appointment/settings.py
+++ BACKEND/Backend_appointment/settings.py
@@ -96,16 +96,34 @@ AUTHENTICATION_BACKENDS = [
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'appointment_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+import environ
+import os
+
+env = environ.Env()
+environ.Env.read_env() 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'appointment_db',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': env('POSTGRES_PORT'),
     }
 }
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
